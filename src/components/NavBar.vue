01db6ffc5b59cf720a8a2da8a56ff30851fcc664
Langat Emmanuel <mannuehkipkirui@gmail.com>
2022-12-24 00:11:10 +0300

    updated the nav-bar css

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -30,6 +30,41 @@ export default {
 <style scoped>
 
 </style>
-<style lang='scss' scoped>
+<style lang='css' scoped>
+.navigation {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    background-color: var(--background-color);
+    padding: 20px 100px 20px 100px;
+    height: var(--navbar-height-large);
+    font-family: var(--font-family-nav);
+}
 
+.navigation .nav-wrapper a {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    color: white;
+    text-decoration: none;
+    font-size: 1.2rem;
+}
+
+.navigation .nav-wrapper span {
+    padding-left: 15px;
+}
+
+.navigation .nav-wrapper img {
+    width: 40px;
+    height: 40px;
+    object-fit: cover;
+    border-radius: 50%;
+}
+
+.navigation nav a {
+    color: var(--navbar-color);
+    text-decoration: none;
+    padding: 0 15px;
+}
 </style>
